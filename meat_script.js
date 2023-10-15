cddb99950505a3b1905078378f0d5184eab39b96
ping <konpah3019@gmail.com>
2023-10-15 11:41:47 +0700

    change id class name wait for integrate

--- meat_script.js
+++ meat_script.js
@@ -14,15 +14,15 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     var progress = 0;
-    const progressBar = document.getElementById('progress');
+    const progressBar = document.getElementById('meat_progress');
     let isIncreasing = false; // Flag to track whether the gauge is increasing
-    var gasSound = document.getElementById('gas-sound');
+    var gasSound = document.getElementById('meat_gas-sound');
 
     // Function to increase the gauge progress over time
     function increaseProgress() {
         
         if (progress < 100 && isIncreasing) {
-            progress += 0.5; // Increase the progress by 1% (adjust as needed)
+            progress += 0.5; // Increase the progress by 1% (adjust as needed) <============== *** Adjust difficulty ***
             progressBar.style.height = progress + '%';
             
             requestAnimationFrame(increaseProgress);
@@ -69,13 +69,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Add a click event to the "Flip" button to toggle the gauge increase
-    document.getElementById('gas-button').addEventListener('click', function () {
+    document.getElementById('meat_gas-button').addEventListener('click', function () {
         
         toggleGauge();
     });
 
     // the game end here
-    document.getElementById('flip-button').addEventListener('click', function () {
+    document.getElementById('meat_flip-button').addEventListener('click', function () {
         
         if (progress >= 70 && progress <= 80) {
             alert('redirect')
